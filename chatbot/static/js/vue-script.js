b4d5174b34b977a56cc788b8f9a962d35804044c
BarnikRay <barnikr06@gmail.com>
2019-05-13 14:34:01 +0530

    add chat window

--- chatbot/static/js/vue-script.js
+++ chatbot/static/js/vue-script.js
@@ -1,13 +1,32 @@
+let json = null;
 window.addEventListener('load', function () {
     $.ajax({
         type: 'json',
         url: '/static/json/queries.json',
         success: function (data) {
-            var vm = new Vue({
-                delimiters: ['[[', ']]'],
-                el: '#app',
-                data: {query: data}
-            });
+            json = data;
+        }
+    });
+    var vm = new Vue({
+        delimiters: ['[[', ']]'],
+        el: '#app',
+        data: {messages: []},
+        methods: {
+            addMessage: function () {
+                var message = {
+                    "user": true,
+                    "bot": false,
+                    "text": 'yo yo'
+                }
+                this.messages.push(message);
+                alert('yo');
+                var message1 = {
+                    "user": false,
+                    "bot": true,
+                    "text": 'yo yo'
+                }
+                this.messages.push(message1);
+            }
         }
     });
 });
