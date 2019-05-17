4b6c3383bc07a76339c48de938b14d19826c1d3a
BarnikRay <barnikr06@gmail.com>
2019-05-17 21:20:07 +0530

    add categories

--- chatbot/static/js/vue-script.js
+++ chatbot/static/js/vue-script.js
@@ -5,27 +5,86 @@ window.addEventListener('load', function () {
         url: '/static/json/queries.json',
         success: function (data) {
             json = data;
+            vm.addMessage(false, false, 'Hello!');
+            vm.addMessage(false, true);
         }
     });
     var vm = new Vue({
         delimiters: ['[[', ']]'],
         el: '#app',
-        data: {messages: []},
+        data: {
+            messages: [],
+            category: null,
+            level: 0,
+            subcategory: null,
+            question: null,
+        },
         methods: {
-            addMessage: function () {
-                var message = {
-                    "user": true,
-                    "bot": false,
-                    "text": 'Hey!'
+            addMessage: function (isUser, isOptions, message) {
+                let path = [this.category, this.subcategory, this.question];
+                if (isUser) {
+                    let message = {
+                        "user": true,
+                        "bot": false,
+                        "text": message,
+                        "options": false
+                    };
+                    this.messages.push(message);
+                } else {
+                    if (isOptions) {
+                        let message = [];
+                        let jsonVal = json['categories'];
+                        console.log(jsonVal);
+
+                        for (let i = 0; i < this.level; i++) {
+                            jsonVal = jsonVal[path[i]];
+                        }
+                        for (keys in jsonVal) {
+                            message.push(keys);
+                        }
+                        let message1 = {
+                            "user": false,
+                            "bot": true,
+                            "text": message,
+                            "options": true
+                        };
+                        this.messages.push(message1);
+                    } else {
+                        var message1 = {
+                            "user": false,
+                            "bot": true,
+                            "text": message,
+                            "options": false
+                        }
+                        this.messages.push(message1);
+                    }
                 }
-                this.messages.push(message);
-                var message1 = {
-                    "user": false,
-                    "bot": true,
-                    "text": 'Hello!'
+            },
+            setCategory: function (category) {
+                this.messages.pop();
+                this.addMessage(true, false, category);
+                if (category !== 'Others') {
+                    switch (this.level) {
+                        case 0:
+                            this.category = category;
+                            break;
+                        case 1:
+                            this.subcategory = category;
+                            break;
+                        case 2:
+                            this.question = category;
+                    }
+                    this.level++;
+                    // this.isOptions = true;
+                } else {
+                    this.level = 0;
                 }
-                this.messages.push(message1);
+                this.addMessage(false, true);
+
             }
+        },
+        created: function () {
+            // this.addMessage(false,true);
         }
     });
 });
