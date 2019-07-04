e2ec004fbb52666c85a045d58b798d698d9e83c0
BarnikRay <barnikr06@gmail.com>
2019-07-04 22:09:43 +0530

    minor changes

--- bot/customsearch.py
+++ bot/customsearch.py
@@ -1,11 +1,8 @@
 from googleapiclient.discovery import build
 import os
-import logging
 
 API_KEY = os.environ['API_KEY']
 CSE_ID = os.environ['CSE_ID']
-
-logger=logging.getLogger('chatbot')
 
 
 def fetchResponse(question):
@@ -17,8 +14,6 @@ def fetchResponse(question):
     ).execute()
     res = res['items'][0]
     response = {'link': res['link'], 'title': res['title'], 'desc': res['snippet']}
-    logger.info(question)
-    logger.info(response)
     return response
 
 
