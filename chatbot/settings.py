961c37274abae89582e7065fdc453a7081b46807
BarnikRay <barnikr06@gmail.com>
2019-07-04 22:04:48 +0530

    minor changes

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Session handling
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
